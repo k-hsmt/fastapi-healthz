384cf653ce2f5c4e5d3c69162365b44b2097397b
k-hsmt <k.hsmt@live.com>
2025-09-02 16:39:29 +0900

    chore: enable CORS for localhost:3000

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+origins = [
+    "http://localhost:3000",      # 開発中のNext.js
+    "http://127.0.0.1:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,   # 認証付きfetchを想定しないなら True/FalseどちらでもOK
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/healthz")
 def healthz():
